0675f1463a7780bbbfe121cf7284e2c295b71ecd
Edward Williams <eddie_w38@yahoo.com>
2020-02-04 17:27:41 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,13 @@
-# Your code goes here!
+class Dog
+ def dog.name = "fido"
+ end
+ 
+ def dog
+   @dogs_name_is = "fido"
+ end
+ 
+ def dog
+    dog.new = fido
+      dog.sit = "Fido is sitting"
+      
+    end
